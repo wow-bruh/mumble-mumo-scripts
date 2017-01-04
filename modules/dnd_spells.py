578192e907b159406d7e4313cae4a65e9ea247d0
Flo <wow_bruh@outlook.com>
2017-01-04 18:30:40 +0000

    Fix formatting, fix generators

--- modules/dnd_spells.py
+++ modules/dnd_spells.py
@@ -92,27 +92,34 @@ Range: {spell_range}
         matching_spell = None
         # Check for an exact match first
         if name in self.spells_names:
-            matching_spell = next(spell for spell in self.spells if spell["name"] == name, None)
+            matching_spell = next((spell for spell in self.spells if spell["name"].lower() == name), None)
         if matching_spell is not None:
             return self.get_spell_info(name, matching_spell);
         
         # No direct match, let's attempt a fuzzy match
         matches = process.extract(name, self.spells_names, limit=3)
         if matches is None or len(matches) == 0:
-            return "Well I couldn't even find a single spell in my database. That's bad."
+            return "Well I couldn't even find a single spell in my database that matches that. That's bad."
 
         # Unsure matches go here
         if matches[0][1] < 50:
             msg = "I've found some stuff, but I'm not entirely sure that's what you mean. Mind giving me a bit more info? Those are the spells I found that might match:<br>"
             for item in matches:
-                msg = "<b>"msg + item[0] + "</b><br>"
+                msg = msg + "<b>" + item[0].encode("utf-8").title() + "</b><br>"
             return msg
 
-        matching_spell = next(spell for spell in self.spells if spell["name"] == matches[0][0], None)
+        matching_spell = next((spell for spell in self.spells if spell["name"].lower() == matches[0][0]), None)
         if matching_spell is None:		
-            return "Well I couldn't even find a single spell in my database. That's bad."
+            return "Well I couldn't even find a single spell in my database. That's bad. For the record, I tried to match against " + matches[0][0]
         
-        return self.get_spell_info(name, matching_spell)
+        msg = self.get_spell_info(name, matching_spell)
+	msg = msg + "<br>In case that's wrong, here are some other close matches I have in database:<br>"
+	iterspell = iter(matches)
+	next(iterspell)
+	for item in iterspell:
+		msg = msg + item[0].encode("utf-8").title() + "<br>"
+
+	return msg
 
     def userTextMessage(self, server, user, message, current=None):
         # Ensure we're using the keyword
