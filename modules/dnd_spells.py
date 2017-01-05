7dae9a62e61a24d9ca0c2196d7b2fa103856247f
Wow-Bruh <wow_bruh@outlook.com>
2017-01-05 20:05:19 +0100

    Changed display of things

--- modules/dnd_spells.py
+++ modules/dnd_spells.py
@@ -1,66 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
-
-# Copyright (C) 2011 Stefan Hacker <dd0t@users.sourceforge.net>
-# All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-
-# - Redistributions of source code must retain the above copyright notice,
-#   this list of conditions and the following disclaimer.
-# - Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-# - Neither the name of the Mumble Developers nor the names of its
-#   contributors may be used to endorse or promote products derived from this
-#   software without specific prior written permission.
-
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# `AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-#
-# seen.py
-# This module allows asking the server for the last time it saw a specific player
+# dnd_spells.py
+# This module allows asking the server to look for a spell in dungeons and dragons
 #
 
-from mumo_module import (commaSeperatedIntegers,
-                         MumoModule)
-from datetime import timedelta
 import json
-import operator
+from mumo_module import MumoModule
 from fuzzywuzzy import process
 
 class dnd_spells(MumoModule):
-    
-    def __init__(self, name, manager, configuration = None):
+    def __init__(self, name, manager, configuration=None):
         MumoModule.__init__(self, name, manager, configuration)
         self.murmur = manager.getMurmurModule()
+        # Load spells
         with open("spells.json") as spells_file:
             self.spells = json.loads(spells_file.read())
         self.spells_names = [item["name"].lower() for item in self.spells]
-        
+
+        # Load attribute name -> full length words mapping
+        self.attr_map = {
+            "name": "",
+
+            "page": "Source: ",
+            "range": "Range: ",
+            "components": "Components: ",
+            "material": "Material: ",
+            "ritual": "Is a Ritual",
+            "concentration": "Is a Concentration spell",
+            "casting_time": "Time to cast: ",
+            "level": "Level: ",
+            "class": "Usable by ",
+            "school": "School of ",
+            "circle": "Circle of the ",
+            "archetype": "Archetype: ",
+            "duration": "Duration: ",
+            "domains": "Domains: ",
+            "patrons": "Patrons: ",
+            "oaths": "Oaths: ",
+
+            "desc": ""
+        }
 
     def connected(self):
         manager = self.manager()
         log = self.log()
         log.debug("Register for Server callbacks")
         servers = manager.SERVERS_ALL
-            
         manager.subscribeServerCallbacks(self, servers)
-    
-    def disconnected(self): pass
-    
+
+    def disconnected(self):
+        pass
+
     def sendMessage(self, server, user, message, msg):
         if message.channels:
             server.sendMessageChannel(user.channel, False, msg)
@@ -72,30 +63,22 @@ class dnd_spells(MumoModule):
     #
 
     def get_spell_info(self, search, spell):
-        msg = """
-Here's what I found that's my closest match for {looking_for}:<br><b>
-{spell_name}</b>
-{spell_desc}
-Higher Level: {spell_higher_level}<br>
-Range: {spell_range}
-<br>
-<b>FLO YOU GODDAMN PRICK LIMIT THAT TO DND CHANNEL</b>
-""".format(looking_for = search.encode("utf-8"),
-            spell_name = spell["name"].encode("utf-8"),
-            spell_desc = spell["desc"].encode("utf-8"),
-            spell_higher_level = spell["higher_level"].encode("utf-8") if "higher_level" in spell.keys() else "",
-            spell_range = spell["range"].encode("utf-8") if "range" in spell.keys() else "")
+        spell_keys = spell.keys()
+        msg = """Here's what I found that's my closest match for {looking_for}:<br>""".format(looking_for=search.encode("utf-8"))
+        for k in self.attr_map:
+            if k in spell_keys:
+                msg = msg + self.attr_map[k] + spell[k] + "<br>"
 
         return msg
-    
+
     def fuzzy_match(self, name):
         matching_spell = None
         # Check for an exact match first
         if name in self.spells_names:
             matching_spell = next((spell for spell in self.spells if spell["name"].lower() == name), None)
         if matching_spell is not None:
-            return self.get_spell_info(name, matching_spell);
-        
+            return self.get_spell_info(name, matching_spell)
+
         # No direct match, let's attempt a fuzzy match
         matches = process.extract(name, self.spells_names, limit=3)
         if matches is None or len(matches) == 0:
@@ -109,17 +92,17 @@ Range: {spell_range}
             return msg
 
         matching_spell = next((spell for spell in self.spells if spell["name"].lower() == matches[0][0]), None)
-        if matching_spell is None:		
+        if matching_spell is None:
             return "Well I couldn't even find a single spell in my database. That's bad. For the record, I tried to match against " + matches[0][0]
-        
-        msg = self.get_spell_info(name, matching_spell)
-	msg = msg + "<br>In case that's wrong, here are some other close matches I have in database:<br>"
-	iterspell = iter(matches)
-	next(iterspell)
-	for item in iterspell:
-		msg = msg + item[0].encode("utf-8").title() + "<br>"
 
-	return msg
+        msg = self.get_spell_info(name, matching_spell)
+        msg = msg + "<br>In case that's wrong, here are some other close matches I have in database:<br>"
+        iterspell = iter(matches)
+        next(iterspell)
+        for item in iterspell:
+            msg = msg + item[0].encode("utf-8").title() + "<br>"
+
+        return msg
 
     def userTextMessage(self, server, user, message, current=None):
         # Ensure we're using the keyword
@@ -131,16 +114,21 @@ Range: {spell_range}
         if len(split) != 2:
             return
         name = split[1].lower()
-        
+
         msg = self.fuzzy_match(name)
         server.sendMessageChannel(user.channel, False, msg)
 
-    
-    def userConnected(self, server, state, context = None): pass
-    def userDisconnected(self, server, state, context = None): pass
-    def userStateChanged(self, server, state, context = None): pass
-    
-    def channelCreated(self, server, state, context = None): pass
-    def channelRemoved(self, server, state, context = None): pass
-    def channelStateChanged(self, server, state, context = None): pass
 
+    def userConnected(self, server, state, context=None):
+        pass
+    def userDisconnected(self, server, state, context=None):
+        pass
+    def userStateChanged(self, server, state, context=None):
+        pass
+
+    def channelCreated(self, server, state, context=None):
+        pass
+    def channelRemoved(self, server, state, context=None):
+        pass
+    def channelStateChanged(self, server, state, context=None):
+        pass
